87ffda8dfdb7e9a0a815ed245ac15181e086ca0e
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-06-15 16:53:33 +0530

    connected vercel link to cors

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ connectCloudinary();
 
 //cors is used to connect the frontend to backend
 // allow multiple origins
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = ["http://localhost:5173","https://freshcart-three-eta.vercel.app/"];
 
 app.post("/stripe", express.raw({ type: "application/json" }), stripWebhooks);
 
