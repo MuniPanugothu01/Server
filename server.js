73ede9d7ee2a05035e05503b06d5e639b9bf0e8d
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-06-16 16:55:50 +0530

    cors and server delployed in render

--- server.js
+++ server.js
@@ -25,8 +25,7 @@ connectCloudinary();
 //cors is used to connect the frontend to backend
 // allow multiple origins
 const allowedOrigins = [
-  "http://localhost:5173",
-  "https://freshcart-three-eta.vercel.app",
+  "http://localhost:5173",'https://freshcart-three-eta.vercel.app'
 ];
 
 app.post("/stripe", express.raw({ type: "application/json" }), stripWebhooks);
