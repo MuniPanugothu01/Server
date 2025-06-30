fbf2e2899fa41010939ea46252fd9489c6a82022
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-06-30 16:57:33 +0530

    backto add address

--- controllers/addressController.js
+++ controllers/addressController.js
@@ -1,68 +1,10 @@
-// const { Address } = require("../models/Address.js");
-
-// // Add Address : /api/address/add
-// const addAddress = async (req, res) => {
-//   try {
-//     const {
-//       // userId,
-//       firstName,
-//       lastName,
-//       email,
-//       street,
-//       city,
-//       state,
-//       zipcode,
-//       country,
-//       phone,
-//     } = req.body;
-
-//     // ✅ Use the userId from the authenticated request
-//     const userId = req.userId;
-
-//     await Address.create({
-//       userId,
-//       firstName,
-//       lastName,
-//       email,
-//       street,
-//       city,
-//       state,
-//       zipcode,
-//       country,
-//       phone,
-//     });
-
-//     res.json({ success: true, message: "Address added successfully 😊" });
-//   } catch (error) {
-//     console.log(error.message);
-//     res.json({ success: false, message: error.message });
-//   }
-// };
-
-// // get address : /api/address/get
-// const getAddress = async (req, res) => {
-//   try {
-//     // const { userId } = req.query;
-//     const userId = req.userId;
-//     const addresses = await Address.find({ userId });
-//     res.json({ success: true, addresses });
-//   } catch (error) {
-//     console.log(error.message);
-//     res.json({ success: false, message: error.message });
-//   }
-// };
-
-// module.exports = {
-//   addAddress,
-//   getAddress,
-// };
-
 const { Address } = require("../models/Address.js");
 
 // Add Address : /api/address/add
 const addAddress = async (req, res) => {
   try {
     const {
+      // userId,
       firstName,
       lastName,
       email,
@@ -74,8 +16,8 @@ const addAddress = async (req, res) => {
       phone,
     } = req.body;
 
-    // ✅ Get userId from authUser middleware
-    const userId = req.user.id;
+    // ✅ Use the userId from the authenticated request
+    const userId = req.userId;
 
     await Address.create({
       userId,
@@ -97,12 +39,12 @@ const addAddress = async (req, res) => {
   }
 };
 
-// Get Address : /api/address/get
+// get address : /api/address/get
 const getAddress = async (req, res) => {
   try {
-    const userId = req.user.id;
-    const addresses = await Address.find({ userId });
-
+    // const { userId } = req.query;
+    const userId = req.userId;
+    const addresses = await Address.find({ userId }); 
     res.json({ success: true, addresses });
   } catch (error) {
     console.log(error.message);
--- routes/addressRoute.js
+++ routes/addressRoute.js
@@ -1,27 +1,12 @@
-// const express = require("express");
-// const { authUser } = require("../middlewares/authUser");
-// const { addAddress, getAddress } = require("../controllers/addressController");
-// const addressRouter = express.Router();
-
-// addressRouter.post("/add", authUser, addAddress);
-
-// addressRouter.get("/get", authUser, getAddress);
-
-// module.exports = {
-//     addressRouter,
-// }
 const express = require("express");
 const { authUser } = require("../middlewares/authUser");
 const { addAddress, getAddress } = require("../controllers/addressController");
-
 const addressRouter = express.Router();
 
-// Add address (POST)
 addressRouter.post("/add", authUser, addAddress);
 
-// Get user addresses (GET)
 addressRouter.get("/get", authUser, getAddress);
 
 module.exports = {
-  addressRouter,
-};
+    addressRouter,
+}
